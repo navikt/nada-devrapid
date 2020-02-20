5682eb11261eb5f3cc3fba346506c58b74e3bbe7
Christopher Kolstad <christopher.kolstad@nav.no>
2020-02-20 11:07:56 +0100

    Register callback for kafka producer

--- src/main/kotlin/no/nav/nada/devrapid/KafkaMessageSender.kt
+++ src/main/kotlin/no/nav/nada/devrapid/KafkaMessageSender.kt
@@ -6,11 +6,21 @@ import org.apache.avro.generic.GenericRecord
 import org.apache.kafka.clients.producer.KafkaProducer
 import org.apache.kafka.clients.producer.ProducerRecord
 import com.sksamuel.avro4k.Avro
+import mu.KotlinLogging
 
 class KafkaMessageSender(val topic: String, val kafkaProducer: KafkaProducer<GenericRecord, GenericRecord>) : MessageSender {
+    companion object {
+        val logger = KotlinLogging.logger {}
+    }
     override suspend fun sendDevEvent(devEvent: DevEvent) {
         val value = Avro.default.toRecord(DevEvent.serializer(), devEvent)
         val key = Avro.default.toRecord(NadaResourceNames.serializer(), devEvent.nrn)
-        kafkaProducer.send(ProducerRecord(topic, key, value))
+        kafkaProducer.send(ProducerRecord(topic, key, value), { m, err ->
+            if (err != null) {
+                logger.warn { err }
+            } else {
+                logger.info { "Sent message with offset ${m.offset()} on partition ${m.partition()}" }
+            }
+        })
     }
 }
